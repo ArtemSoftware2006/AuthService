c492938d121d6d3e9998f92d6fdf53caafe99bff
Artem <artem.egorov.art2006@gmail.com>
2023-08-01 16:24:46 +0300

    внедрил авторизацию через jwt bearer в program

--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,38 @@
 using DAL;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var conf_builder = new ConfigurationBuilder();
 
 conf_builder.SetBasePath(Directory.GetCurrentDirectory());
-conf_builder.AddJsonFile("appsettings.json");
+conf_builder.AddJsonFile("security.json");
 var config = conf_builder.Build();
 
 var connection = config.GetConnectionString("DefaultConnection");
-
-Console.WriteLine(connection);
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = config.GetSection("Issuer").Value,
+                ValidAudience = config.GetSection("Audience").Value,
+                IssuerSigningKey = new SymmetricSecurityKey(Convert.FromBase64String(config.GetSection("Secret").Value))
+            };
+        });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
